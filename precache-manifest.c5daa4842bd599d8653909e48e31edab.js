7a95bc75d42ea2671ed089ed2acf4a4673ab9ffb
nyowusu <yaw.owusu@hotmail.com>
2019-07-17 16:32:38 +0100

    Updates

---
+++ precache-manifest.c5daa4842bd599d8653909e48e31edab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce30e033393feea66484256f9aebf065",
+    "url": "/simplecalc/index.html"
+  },
+  {
+    "revision": "752215adf8c69c4997ae",
+    "url": "/simplecalc/static/css/main.9b157bc2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/simplecalc/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "752215adf8c69c4997ae",
+    "url": "/simplecalc/static/js/main.2368a94a.chunk.js"
+  },
+  {
+    "revision": "e767edc71229c3b3f254",
+    "url": "/simplecalc/static/js/runtime~main.a7447eb1.js"
+  }
+]);
